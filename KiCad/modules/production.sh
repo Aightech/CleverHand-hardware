e2a6da6c49d2efb9c3060649ffa8012765dc183f
aightech <long.devillard@gmail.com>
2024-03-30 22:04:03 +0000

    cleaned all modules + production files

---
+++ KiCad/modules/production.sh
@@ -0,0 +1,16 @@
+#bash file to generate production files for all folders
+source ~/.bashrc
+#use the kicad-plot function from the .bashrc file
+export -f kicad-plot
+
+#for each folder in the current directory
+for d in */ ; do
+    #remove the last character of the folder name
+    folder=${d%?}
+    echo -e "Generating production files for \e[1m$folder\e[0m"
+    cd $folder
+    kicad-plot $folder
+    cd ..
+    echo ""
+    
+done
